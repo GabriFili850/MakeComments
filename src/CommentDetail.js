705b52bfa5665e9bfd36813145306f968c8327ae
GabriFili850 <gabriele.filigheddu@hotmail.com>
2019-04-25 16:30:27 +0100

    passing and receving props

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import faker from 'faker';
 
-const CommentDetail = () => {
+const CommentDetail = props => {
+  console.log(props);
   return (
     <div className="comment">
       <a href="/" className="avatar">
@@ -9,7 +10,6 @@ const CommentDetail = () => {
       </a>
       <div className="content">
         <a href="/" className="author">
-        Sam
         </a>
         <div className="metadata">
           <span className="date">Today at 6:00PM</span>
